5bfe491bc3647acea48a3e23f859b4cc6c5804c0
xin599 <313157137@qq.com>
2020-06-04 22:38:53 +0800

    vuex练习

--- project-lxy/src/store/index.js
+++ project-lxy/src/store/index.js
@@ -5,11 +5,30 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    count: 1
+    count: 9
   },
   mutations: {
+    add(state,n){
+      state.count+=n
+    },
+    reduce(state){
+      state.count--
+    }
+  },
+  getters:{
+    count:state=>state.count+=0
   },
   actions: {
+    addAction(context){
+      context.commit('add',10)
+      setTimeout(()=>{
+        context.commit('reduce')
+      },2000)
+      console.log('我是先执行的')
+    },
+    reduceAction({commit}){
+      commit('reduce')
+    }
   },
   modules: {
   }
--- project-lxy/src/views/Count.vue
+++ project-lxy/src/views/Count.vue
@@ -1,7 +1,48 @@
 <template>
-
+    <div>
+        <h2>{{msg}}</h2><hr/>
+        <h3>{{$store.state.count}}-{{count}}</h3>
+        <p>
+            <button @click="$store.commit('add',5)">+</button>
+            <button @click="reduce">-</button>
+            <!-- <button @click="$store.commit('reduce')">-</button> -->
+        </p>
+        <p>
+            <button @click="addAction">+</button>
+            <button @click="reduceAction">-</button>
+        </p>
+    </div> 
 </template>
 
 <script>
-import store from '@store/index';
+import store from '../store/index';
+import { mapState, mapMutations, mapGetters, mapActions } from 'vuex'
+export default{
+    data(){
+        return {
+            msg:'Hello Vuex'
+        }
+    },
+    // computed:{
+    //     count(){
+    //         return this.$store.state.count
+    //     }
+    // },
+    // computed:mapState({
+    //     count:state => state.count
+    // }),
+    // computed:mapState(['count']),
+    computed:{
+        ...mapState(['count']),
+        // count(){
+        //     return this.$store.getters.count
+        // },
+        ...mapGetters(['count'])
+    },
+    methods:{
+        ...mapMutations(['add','reduce']),
+        ...mapActions(['addAction','reduceAction'])
+    },
+    store
+}
 </script>
