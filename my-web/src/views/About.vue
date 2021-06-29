7125b4fbd45356989f81e22a7a06bc4ba3fb6ca7
lxy <313157137@qq.com>
2021-06-29 17:10:30 +0800

    添加输入框和统计图

--- my-web/src/views/About.vue
+++ my-web/src/views/About.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>统计图</h1>
+    <div
+      id="myChart"
+      :style="{width: '300px', height: '300px'}"
+    ></div>
   </div>
 </template>
+<script>
+var echarts = require('echarts')
+export default {
+  data() {
+    return {}
+  },
+  mounted() {
+    this.drawLine()
+  },
+  methods: {
+    drawLine() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = echarts.init(document.getElementById('myChart'))
+      // 绘制图表
+      myChart.setOption({
+        title: { text: '统计图' },
+        tooltip: {},
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20],
+          },
+        ],
+      })
+    },
+  },
+}
+</script>
--- my-web/src/views/Home.vue
+++ my-web/src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <select>
+        <option>---请选择---</option>
+        <option>0</option>
+        <option>1</option>
+      </select>
+    </div>
+    <div>
+      <div
+        :key="index"
+        v-for="(item,index) in list"
+      >
+        <span>{{item.name1}}</span><input type="text"><span>{{item.name2}}</span><input type="text">
+        <button
+          v-if="index+1===list.length"
+          @click="addItem"
+        >添加</button>
+        <button
+          v-if="index!==0"
+          @click="delItem(index)"
+        >删除</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      list: [{ name1: 'name1', name2: 'name2' }],
+    }
+  },
+  methods: {
+    addItem() {
+      this.list.push({ name1: 'name1', name2: 'name2' })
+    },
+    delItem(i) {
+      this.list.splice(i, 1)
+    },
+  },
 }
 </script>
