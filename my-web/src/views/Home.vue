5538d9f913d23b5de7aa4477b000f9d864ce681b
xin599 <313157137@qq.com>
2021-06-30 00:33:06 +0800

    样式修改

--- my-web/src/views/Home.vue
+++ my-web/src/views/Home.vue
@@ -1,26 +1,21 @@
 <template>
   <div class="home">
     <div>
-      <select>
-        <option>---请选择---</option>
-        <option>0</option>
-        <option>1</option>
-      </select>
-    </div>
-    <div>
-      <div
-        :key="index"
-        v-for="(item,index) in list"
-      >
-        <span>{{item.name1}}</span><input type="text"><span>{{item.name2}}</span><input type="text">
-        <button
-          v-if="index+1===list.length"
-          @click="addItem"
-        >添加</button>
-        <button
-          v-if="index!==0"
-          @click="delItem(index)"
-        >删除</button>
+      <div>
+        <select>
+          <option>0</option>
+          <option>1</option>
+        </select>
+      </div>
+      <div class="input-box">
+        <div :key="index" v-for="(item, index) in list">
+          <span class="name">{{ item.name1 }}</span><input class="input" type="text" />
+          <span class="name">{{ item.name2 }}</span><input class="input" type="text" />
+          <button v-if="index + 1 === list.length" @click="addItem">
+            添加
+          </button>
+          <!-- <button v-if="index !== 0" @click="delItem(index)">删除</button> -->
+        </div>
       </div>
     </div>
   </div>
@@ -28,19 +23,40 @@
 
 <script>
 export default {
-  name: 'Home',
+  name: "Home",
   data() {
     return {
-      list: [{ name1: 'name1', name2: 'name2' }],
-    }
+      list: [{ name1: "板块/个股", name2: "数值" }],
+    };
   },
   methods: {
     addItem() {
-      this.list.push({ name1: 'name1', name2: 'name2' })
+      this.list.push({ name1: "板块/个股", name2: "数值" });
     },
     delItem(i) {
-      this.list.splice(i, 1)
+      this.list.splice(i, 1);
     },
   },
-}
+};
 </script>
+
+<style lang="scss" scoped>
+.input-box{
+  width: 600px;
+  margin: 10px auto 0;
+  padding: 10px 0;
+  background-color: #f2f2f2;
+  box-shadow: 8px 8px 10px rgba($color: #000000, $alpha: 0.2);
+  text-align: left;
+  .name{
+    margin-right: 20px;
+  }
+  .input{
+    margin-right: 20px;
+    border-radius: 3px;
+    &:hover{
+      border-color: rgb(53, 144, 230);
+    }
+  }
+}
+</style>
