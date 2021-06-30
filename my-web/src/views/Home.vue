9a39104c4aa253f157337220cea783b45ef60d79
lxy <313157137@qq.com>
2021-06-30 14:36:57 +0800

    样式修改

--- my-web/src/views/Home.vue
+++ my-web/src/views/Home.vue
@@ -2,18 +2,30 @@
   <div class="home">
     <div>
       <div>
-        <select>
+        <select class="select">
           <option>0</option>
           <option>1</option>
         </select>
       </div>
       <div class="input-box">
-        <div :key="index" v-for="(item, index) in list">
-          <span class="name">{{ item.name1 }}</span><input class="input" type="text" />
-          <span class="name">{{ item.name2 }}</span><input class="input" type="text" />
-          <button v-if="index + 1 === list.length" @click="addItem">
-            添加
-          </button>
+        <div
+          :key="index"
+          v-for="(item, index) in list"
+          class="input-item"
+        >
+          <span class="name">{{ item.name1 }}</span><input
+            class="input"
+            type="text"
+          />
+          <span class="name">{{ item.name2 }}</span><input
+            class="input"
+            type="text"
+          />
+          <div
+            class="add"
+            v-if="index + 1 === list.length"
+            @click="addItem"
+          >+</div>
           <!-- <button v-if="index !== 0" @click="delItem(index)">删除</button> -->
         </div>
       </div>
@@ -23,39 +35,56 @@
 
 <script>
 export default {
-  name: "Home",
+  name: 'Home',
   data() {
     return {
-      list: [{ name1: "板块/个股", name2: "数值" }],
-    };
+      list: [{ name1: '板块/个股', name2: '数值' }],
+    }
   },
   methods: {
     addItem() {
-      this.list.push({ name1: "板块/个股", name2: "数值" });
+      this.list.push({ name1: '板块/个股', name2: '数值' })
     },
     delItem(i) {
-      this.list.splice(i, 1);
+      this.list.splice(i, 1)
     },
   },
-};
+}
 </script>
 
 <style lang="scss" scoped>
-.input-box{
+.select {
+  width: 60px;
+  height: 24px;
+}
+.input-box {
   width: 600px;
   margin: 10px auto 0;
-  padding: 10px 0;
+  padding: 10px;
   background-color: #f2f2f2;
   box-shadow: 8px 8px 10px rgba($color: #000000, $alpha: 0.2);
   text-align: left;
-  .name{
-    margin-right: 20px;
-  }
-  .input{
-    margin-right: 20px;
-    border-radius: 3px;
-    &:hover{
-      border-color: rgb(53, 144, 230);
+  .input-item {
+    margin-bottom: 6px;
+    .name {
+      margin-right: 20px;
+    }
+    .input {
+      margin-right: 20px;
+      border-radius: 3px;
+      &:hover {
+        border-color: rgb(53, 144, 230);
+      }
+    }
+    .add {
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      background: #fff;
+      line-height: 20px;
+      text-align: center;
+      cursor: pointer;
     }
   }
 }
